b5b031b75a34d6bfdb41650db2e9552713c1154d
Sashank-Shah <thinkofit452@gmail.com>
2023-02-01 03:39:28 -0800

    Added SPI send data

---
+++ ShahSashankCode/Drivers/Inc/spi_stm32f4xx.h
@@ -0,0 +1,174 @@
+/*
+ * spi_stm32f4xx.h
+ *
+ *  Created on: Jan 29, 2023
+ *      Author: sashank.shah
+ */
+
+#ifndef INC_SPI_STM32F4XX_H_
+#define INC_SPI_STM32F4XX_H_
+#include "STM32F407xx.h"
+#include <stdint.h>
+
+/*
+ * @SPI Mode Select
+ */
+
+#define SLAVE 					0
+#define MASTER 					1
+
+
+/*
+ * @SPI communication
+ */
+
+#define FULL_DUPLEX				1
+#define HALF_DUPLEX				2
+#define SIMPLEX_RX				3
+
+
+/*
+ * @Data frame format
+ */
+
+#define BIT_FRAME_8				0
+#define BIT_FRAME_16			1
+
+
+/*
+ * @SPI CLK Speed
+ */
+
+#define CLK_DIV2				0
+#define CLK_DIV4				1
+#define CLK_DIV8				2
+#define CLK_DIV16				3
+#define CLK_DIV32				4
+#define CLK_DIV64				5
+#define CLK_DIV128				6
+#define CLK_DIV256				7
+
+/*
+ * @SPI CPOL
+ */
+
+#define CPOL_LOW				0
+#define CPOL_HIGH				1
+
+
+
+/*
+ * @SPI CPHA
+ */
+
+#define CPHA_LOW				0
+#define CPHA_HIGH				1
+
+
+/*
+ * @SLAVE MANAGE
+ */
+
+#define SLAVE_SW_DS				0
+#define SLAVE_SW_EN				1
+
+/*
+ * SPI_CR1 Bit fields
+ */
+
+#define CPHA				0
+#define CPOL				1
+#define MSTR				2
+#define BR					3
+#define SPE					6
+#define LSB_FIRST			7
+#define SSI					8
+#define SSM					9
+#define RX					10
+#define DFF					11
+#define CRC_NEXT			12
+#define CRC_EN				13
+#define BIDI_OE				14
+#define BIDI_MODE			15
+
+
+/*
+ * SPI_CR2 Bit fields
+ */
+
+#define RXDMAEN				0
+#define TXDMAEN				1
+#define SSOE				2
+#define FRF					4
+#define ERRIE				5
+#define RXNEIE				6
+#define TXEIE				7
+
+
+/*
+ * SPI_SR Bit fields
+ */
+#define RXNE				0
+#define TXE					1
+#define CHSIDE				2
+#define UDR					3
+#define CRC_ERR				4
+#define MODF				5
+#define OVR					6
+#define BSY					7
+#define FRE					8
+
+/*----------------- User entered data for SPI --------------------------*/
+typedef struct
+{
+	uint8_t SPI_MODE;			//Master or Slave mode select
+	uint8_t	SPI_COM;			//Half duplex, simplex, or full duplex (communication type)
+	uint8_t	SPI_DFF;			//Data frame format (8 bits shift register or 16 bits shift register)
+	uint8_t	SPI_CPHA;			//Phase select for SPI
+	uint8_t SPI_CPOL;			//Polarity select for SPI
+	uint8_t	SPI_SM;				//Either software or hardware slave management
+	uint8_t	SPI_Speed;			//SPI speed setting
+
+}spi_Config_t;
+
+
+
+
+/*--------------- Handling structure--------------------------*/
+typedef struct
+{
+	spi_RegDef_t *pSPI;
+	spi_Config_t spiConf;
+
+}spi_Handler_t;
+
+
+
+void SPI_ClkCtrl(spi_RegDef_t *pSPI, uint8_t ENorDS);
+
+/************* Initialization and De-initialization API **************/
+
+void SPI_init(spi_Handler_t *pSPI);
+
+
+void SPI_DInit(spi_RegDef_t *pSPI);
+
+void SSI_EN(spi_RegDef_t *pSPI, uint8_t ENorDS);
+
+
+void SPI_START(spi_RegDef_t *pSPI, uint8_t ENorDS);
+void SPI_STOP(spi_RegDef_t *pSPI, uint8_t ENorDS);
+
+/*----------------------SPI Read data coming to the device--------------------------------------*/
+//void SPI_Read(spi_RegDef_t *pSPI);
+
+/*----------------------SPI Send data from the device-------------------------------------*/
+void SPI_Send(spi_RegDef_t *pSPI, uint8_t *data, uint32_t len);
+
+
+
+
+/*----------------------SPI interrupt handling APIs-----------------*/
+
+
+#endif /* INC_SPI_STM32F4XX_H_ */
---
+++ ShahSashankCode/Drivers/Src/spi_stm32f4xx.c
@@ -0,0 +1,161 @@
+/*
+ * spi_stm32f4xx.c
+ *
+ *  Created on: Jan 29, 2023
+ *      Author: sashank.shah
+ */
+#include "spi_stm32f4xx.h"
+
+void SPI_ClkCtrl(spi_RegDef_t *pSPI, uint8_t ENorDS)
+{
+	if(ENorDS == ENABLE)
+	{
+		if(pSPI == SPI1)
+		{
+			SPI1_EN();
+		}
+		else if(pSPI == SPI2)
+		{
+			SPI2_EN();
+		}
+		else if(pSPI == SPI3)
+		{
+			SPI3_EN();
+		}
+
+	}
+	else
+	{
+		if(pSPI == SPI1)
+		{
+			SPI1_DS();
+		}
+		else if(pSPI == SPI2)
+		{
+			SPI2_DS();
+		}
+		else if(pSPI == SPI3)
+		{
+			SPI3_DS();
+		}
+	}
+
+
+}
+
+void SSI_EN(spi_RegDef_t *pSPI, uint8_t ENorDS)
+{
+	if(ENorDS == ENABLE)
+	{
+		pSPI->SPI_CR1 |= (1 << SSI);
+	}
+	else
+	{
+		pSPI->SPI_CR1 &= ~(1 << SSI);
+	}
+}
+
+
+void SPI_START(spi_RegDef_t *pSPI, uint8_t ENorDS)
+{
+	if(ENorDS == ENABLE)
+	{
+		pSPI->SPI_CR1 |= (1 << SPE);
+	}
+	else
+	{
+		pSPI->SPI_CR1 &= ~(1 << SPE);
+	}
+
+}
+
+
+
+void SPI_STOP(spi_RegDef_t *pSPI, uint8_t ENorDS)
+{
+
+	if(ENorDS == DISABLE)
+	{
+		pSPI->SPI_CR1 &= ~(1 << SPE);
+	}
+
+}
+
+
+
+void SPI_init(spi_Handler_t *spiHandler_p)
+{
+	uint32_t bitmask = 0;
+
+	bitmask |= spiHandler_p->spiConf.SPI_MODE << MSTR;
+
+	if(spiHandler_p->spiConf.SPI_COM == FULL_DUPLEX)
+	{
+		bitmask &= ~(1 << BIDI_MODE);		// Full duplex
+	}
+	else if(spiHandler_p->spiConf.SPI_COM == HALF_DUPLEX)
+	{
+		bitmask |= (1 << BIDI_MODE);		// 1 line bidirectional (half duplex)
+	}
+	else if(spiHandler_p->spiConf.SPI_COM == SIMPLEX_RX)
+	{
+		bitmask &= ~(1 << BIDI_MODE); // Full duplex with TX disabled and RX enable (simplex receive)
+		bitmask |= (1 << RX);		  // Enable only RX in Master and forces the SCL from the master to output clock even if the MOSI is disabled
+	}
+
+	bitmask |= (spiHandler_p->spiConf.SPI_Speed << BR); //Baud rate settings
+	bitmask |= (spiHandler_p->spiConf.SPI_DFF << DFF);  //Data frame format
+	bitmask |= (spiHandler_p->spiConf.SPI_CPOL << CPOL);
+	bitmask |= (spiHandler_p->spiConf.SPI_CPHA << CPHA);
+	spiHandler_p->pSPI->SPI_CR1 |= bitmask;
+}
+
+
+void SPI_DInit(spi_RegDef_t *pSPI)
+{
+	if(pSPI == SPI1)
+	{
+		SPI1_RESET();
+	}
+	else if(pSPI == SPI2)
+	{
+		SPI2_RESET();
+	}
+	else if(pSPI == SPI3)
+	{
+		SPI3_RESET();
+	}
+}
+
+
+void SPI_Send(spi_RegDef_t *pSPI, uint8_t *data, uint32_t len)
+{
+	//loop till the data is empty
+	while(len>0)
+	{
+		//wait until the TXE buffer is empty
+
+		while(!(pSPI->SPI_SR & (1<<TXE)));
+
+		//if DFF is set it means that the 16BIT data frame is selected
+		if(pSPI->SPI_CR1 & (1 << DFF))
+		{
+			pSPI->SPI_DR = *((uint16_t *)data);
+			len--;
+			len--;
+			(uint16_t *)data++;
+
+		}
+		//if DFF is set it means that the 16BIT data frame is selected
+		else if ((pSPI->SPI_CR1 & (1 << DFF)) == 0)
+		{
+			pSPI->SPI_DR = *data;
+			len--;
+			data++;
+		}
+
+	}
+}
+
+
+
